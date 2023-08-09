79e60582eea0bb8e22eb53dae4ccae3ee7ad365c
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-08-09 12:36:10 -0500

    learning dependency project
    here a learning manage dependency project, create, edit, update, and use dependency. also i learning a good practice for project use npm

---
+++ test.js
@@ -0,0 +1,12 @@
+//Testing a dependenci
+
+var figlet = require("figlet");
+
+figlet("learning npm", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
